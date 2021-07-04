cb4c3b3ef485a45e571278ce571a01bc8d0ca41d
Ryan Tang <tangryanz@yahoo.ca>
2021-07-04 16:36:56 -0400

    Add comments detailing how to use search API

--- app/search.py
+++ app/search.py
@@ -11,6 +11,10 @@ SELECT_VEHICLE_BASE_QUERY = "SELECT posts.vehicle_id, posts.user_id, post_id, ma
                             "INNER JOIN users ON posts.user_id = users.user_id"
 
 
+# Calling this endpoint requires form data (a dictionary) in the request body
+# The manufacturer, price_low, and price_high keys must exist regardless of whether they have values or not
+# Returns a JSON dictionary with key "data" set to a list of vehicle information made my "manufacturer" and having
+# price <= "price_high" and >= "price_low"
 @bp.route('/select_vehicles', methods=['GET'])
 def select_vehicles():
     if request.method == 'GET':
@@ -36,6 +40,10 @@ def select_vehicles():
         return jsonify({"data": data})
 
 
+# Calling this endpoint requires form data (a dictionary) in the request body
+# "manufacturer" must be present as a key in the request body, set to the manufacturer name
+# Returns a JSON dictionary with key "average_price" set to the average price of the given manufacturer
+# If there was an error, will return a dictionary with key "error" set to the error message
 @bp.route('/get_manufacturer_average_price', methods=['GET'])
 def get_manufacturer_average_price():
     if request.method == 'GET':
