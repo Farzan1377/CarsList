d5b4c9a306df40064a784ae0d73ab1ed90b40671
Chanchev Mahendran <c2mahend@uwaterloo.ca>
2021-07-04 12:38:38 -0400

    vehicle edit

--- app/vehicles.py
+++ app/vehicles.py
@@ -17,7 +17,6 @@ def get_user_vehicles():
         cursor = get_db().cursor()
         cursor.execute("SELECT * FROM vehicles where user_id = %s", (user_id,))
         columns = [col[0] for col in cursor.description]
-        print(columns)
         head_rows = [dict(zip(columns, row)) for row in cursor.fetchmany(size=50)]
 
         return jsonify({"success": head_rows})
@@ -103,6 +102,60 @@ def create_user_vehicle():
         return jsonify({"Success": "vehicle successfully created"})
     else:
         return jsonify(({"error": "Did not insert vehicle"}))
+
+@bp.route('/update_user_vehicles', methods=['PUT'])
+def update_user_vehicle():
+    vehicle_request = request.form
+    vehicle_id = vehicle_request["vehicle_id"]
+    cursor = get_db().cursor()
+    cursor.execute("SELECT * FROM vehicles where vehicle_id = %s", (vehicle_id,))
+    columns = [col[0] for col in cursor.description]
+    head_rows = [dict(zip(columns, row)) for row in cursor.fetchmany(size=50)]
+    if "manufacturer" in vehicle_request:
+        manufacturer = vehicle_request["manufacturer"]
+    else:
+        manufacturer = head_rows[0]["manufacturer"]
+    if "model" in vehicle_request:
+        model = vehicle_request["model"]
+    else:
+        model = head_rows[0]["model"]
+    if "year" in vehicle_request:
+        year = int(vehicle_request["year"])
+    else:
+        year = head_rows[0]["year"]
+    if "type" in vehicle_request:
+        vehicle_type = vehicle_request["type"]
+    else:
+        vehicle_type = head_rows[0]["year"]
+    if "vehicle_condition" in vehicle_request:
+        vehicle_condition = vehicle_request["vehicle_condition"]
+    else:
+        vehicle_condition = head_rows[0]["vehicle_condition"]
+    if "odometer" in vehicle_request:
+        odometer = int(vehicle_request["odometer"])
+    else:
+        odometer = head_rows[0]["odometer"]
+    if "paint_color" in vehicle_request:
+        paint_color = vehicle_request["paint_color"]
+    else:
+        paint_color = head_rows[0]["paint_color"]
+    if "image_url" in vehicle_request:
+        image_url = vehicle_request["image_url"]
+    else:
+        image_url = head_rows[0]["image_url"]
+    if "description" in vehicle_request:
+        description = vehicle_request["description"]
+    else:
+        description = head_rows[0]["description"]
+
+    cursor = get_db().cursor()
+    rows = cursor.execute("UPDATE vehicles set manufacturer = %s, model = %s,year=%s, type = %s, vehicle_condition= %s,"
+                          "odometer = %s, paint_color = %s, image_url= %s, description = %s where "
+                          "vehicle_id = %s", (manufacturer, model,year, vehicle_type,
+                    vehicle_condition,odometer, paint_color, image_url, description,vehicle_id,))
+    get_db().commit()
+    cursor.close()
+    return jsonify({"Success": "vehicle successfully created"})
 
 
 @bp.route('/user_posts', methods=('GET', 'POST', 'DELETE'))
